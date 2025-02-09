1d344e1b428179fb6f9db2f1b770cbf9be859507
ritazhan <zhan.rita.info@gmail.com>
2025-02-09 00:48:19 -0500

    chore: rewrote open and closer drawer code

--- Assets/Scripts/OpenDrawer.cs
+++ Assets/Scripts/OpenDrawer.cs
@@ -1,70 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class OpenBox : MonoBehaviour
 {
 
     [Header("Movement")]
     // movement speed
-    public float openDistance = 0.1f;
+    public float openDistance = 2.1f;
+    public float moveSpeed = 1f;
 
+    private Vector3 originalPos;
+    private Vector3 openPos;
+    private bool drawerCurrentlyOpen;
+    private bool openningDrawer;
 
-    private bool boxOpened;
-    private bool coroutineAllowed;
-    private Vector3 initialPosition;
-
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        boxOpened = false;
-        coroutineAllowed = true;
-        initialPosition = transform.position;
+        timer = Time.time;
+        // initialPosition = transform.position;
+
+        drawerCurrentlyOpen = false;
+
+        originalPos = transform.localPosition;
+
+        openPos = new Vector3(transform.localPosition.x + openDistance,
+            transform.localPosition.y,
+            transform.localPosition.z);
     }
 
-    private void OnMouseDown()
+    void OnMouseDown() 
     {
-        Invoke("RunCoroutine", 0f);
+        openningDrawer = true;
     }
 
-    private void RunCoroutine()
+    void Update()
     {
-        StartCoroutine("OpenThatDoor");
-    }
+        if (openningDrawer) {
+            if (!drawerCurrentlyOpen) {
+                transform.localPosition = Vector3.Lerp(originalPos, openPos, Time.deltaTime * moveSpeed);
+                drawerCurrentlyOpen = true;
+            } else {
+                transform.localPosition = Vector3.Lerp(openPos, originalPos, Time.deltaTime * moveSpeed);
+                drawerCurrentlyOpen = false;
+            }
+            openningDrawer = false;
+        }
 
-    private IEnumerator OpenThatDoor()
-    {
-        // if(Input.GetMouseButtonDown(0)){
-        //     Debug.Log("Held");
+        // if (timer == Time.deltaTime * moveSpeed) {
         // }
-        coroutineAllowed = false;
-
-        if (!boxOpened)
-        {
-            for (float i = 0f; i <= 1f; i += openDistance)
-            {
-                // go forwards on the x axis
-                transform.localPosition = new Vector3(transform.localPosition.x + openDistance,
-                    transform.localPosition.y,
-                    transform.localPosition.z);
-                yield return new WaitForSeconds(0f);
-            }
-            boxOpened = true;
-        }
-        else
-        {
-            for (float i = 1f; i >= 0f; i -= openDistance)
-            {
-                // go backwards on the x axis
-                transform.localPosition = new Vector3(transform.localPosition.x - openDistance,
-                    transform.localPosition.y,
-                    transform.localPosition.z);
-                yield return new WaitForSeconds(0f);
-            }
-            transform.position = initialPosition;
-            boxOpened = false;
-        }
-
-        coroutineAllowed = true;
     }
 }
